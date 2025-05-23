111de5f755f2332a9ebaba386c94fed74cbcaf53
senseibelbi <ivodzidrovski@hotmail.com>
2025-05-23 14:44:04 +0200

    Revert "Change license to Apache 2.0 and update README"
    This reverts commit 6cace4a5d255aa92785a8a7cc62f966d4151fe5f.

--- run_with_logging.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Record the command being executed
-echo "$(date): Running command: $*" >> logs/session.log
-
-# Execute the command and capture both stdout and stderr to session.log
-"$@" 2>&1 | tee -a logs/session.log
